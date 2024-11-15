d44fc2d68a734adb40047491e9ff434183651aed
Stephan <sschweitzer2@myges.fr>
2024-11-15 21:02:59 +0100

    full authentification

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
-import NextAuth, {type NextAuthOptions} from 'next-auth'
-import CredentialsProvider from 'next-auth/providers/credentials'
-import {prisma} from "@/lib/prisma";
+import { prisma } from '@/lib/prisma'
 import { compare } from 'bcrypt'
+import NextAuth, { type NextAuthOptions } from 'next-auth'
+import CredentialsProvider from 'next-auth/providers/credentials'
 
 export const authOptions: NextAuthOptions = {
     session: {
@@ -16,7 +16,7 @@ export const authOptions: NextAuthOptions = {
                     type: 'email',
                     placeholder: 'hello@example.com'
                 },
-                password: {label: 'Password', type: "password"}
+                password: { label: 'Password', type: 'password' }
             },
             async authorize(credentials) {
                 if (!credentials?.email || !credentials.password) {
@@ -24,7 +24,7 @@ export const authOptions: NextAuthOptions = {
                 }
 
                 const user = await prisma.user.findUnique({
-                    where : {
+                    where: {
                         email: credentials.email
                     }
                 })
@@ -33,7 +33,10 @@ export const authOptions: NextAuthOptions = {
                     return null
                 }
 
-                const isPasswordValid = await compare(credentials.password, user.password)
+                const isPasswordValid = await compare(
+                    credentials.password,
+                    user.password
+                )
 
                 if (!isPasswordValid) {
                     return null
@@ -42,13 +45,38 @@ export const authOptions: NextAuthOptions = {
                 return {
                     id: user.id + '',
                     email: user.email,
-                    name: user.name
+                    name: user.name,
+                    randomKey: 'Hey cool'
                 }
-
             }
         })
-    ]
+    ],
+    callbacks: {
+        session: ({ session, token }) => {
+            console.log('Session Callback', { session, token })
+            return {
+                ...session,
+                user: {
+                    ...session.user,
+                    id: token.id,
+                    randomKey: token.randomKey
+                }
+            }
+        },
+        jwt: ({ token, user }) => {
+            console.log('JWT Callback', { token, user })
+            if (user) {
+                const u = user as unknown as any
+                return {
+                    ...token,
+                    id: u.id,
+                    randomKey: u.randomKey
+                }
+            }
+            return token
+        }
+    }
 }
 
 const handler = NextAuth(authOptions)
-export { handler as GET, handler as POST}
+export { handler as GET, handler as POST }
--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,15 @@ import {prisma} from "@/lib/prisma"
 import {authOptions} from "@/app/api/auth/[...nextauth]/route";
 import {getServerSession } from "next-auth"
 import {User} from "@/app/user";
+import {LoginButton, LogoutButton} from "@/app/auth";
 
 export default async function Home() {
   const session = await getServerSession(authOptions)
 
   return (
     <main>
+        <LoginButton/>
+        <LogoutButton/>
           <h2>Server Session</h2>
           <pre>{JSON.stringify(session)}</pre>
           <h2>Client Call</h2>
