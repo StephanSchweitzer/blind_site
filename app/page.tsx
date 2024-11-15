6180a01db3fe8f1cc8b54725b536ac26c73ee27d
Stephan <sschweitzer2@myges.fr>
2024-11-15 18:06:32 +0100

    authentification working

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
 import {prisma} from "@/lib/prisma"
+import {authOptions} from "@/app/api/auth/[...nextauth]/route";
+import {getServerSession } from "next-auth"
+import {User} from "@/app/user";
 
 export default async function Home() {
-  const user = await prisma.user.findFirst({
-    where: {
-      email: 'test@test.com'
-    }
-  })
+  const session = await getServerSession(authOptions)
 
-  return <main>
-    Hello, {user?.name}
-  </main>
+  return (
+    <main>
+          <h2>Server Session</h2>
+          <pre>{JSON.stringify(session)}</pre>
+          <h2>Client Call</h2>
+          <User />
+    </main>
+  )
+
 }
